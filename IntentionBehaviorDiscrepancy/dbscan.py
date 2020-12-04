5126ff8059a948aa3dff55ce068bc150ac5ff668
shafeeka <shafeekar.2018@sis.smu.edu.sg>
2020-12-04 15:27:16 +0800

    Function to compute permission frequency
    Function added to append DBSCAN cluster label to each widget. This version is also able to output the permission frequency count for each DBSCAN cluster (in each hierarchical cluster) as text files.

--- IntentionBehaviorDiscrepancy/dbscan.py
+++ IntentionBehaviorDiscrepancy/dbscan.py
@@ -31,9 +31,41 @@ def load_pkl_data(path):
     with open(path, 'rb') as fi:
         data = pickle.load(fi)
     return data
+# ==============================
+# Basic Anomaly Processing
+# ==============================
+def permission_freq(cluster_dict):
+    path_current = os.path.dirname(os.path.abspath(__file__))
+    path_to_txt =  os.path.join(path_current, 'permission_frequency_42_clusters.txt')
+    f = open(path_to_txt, "a")
+
+    for key in cluster_dict.keys():
+        f.write("Hierarchical Cluster Number: " + str(key+1) + "\n") #apppend hierarchical cluster label
+        perm_dict = {}
+        cluster_data = cluster_dict[key]
+        for i in range(len(cluster_data)):
+            dbscan_label = cluster_data[i][2]
+            apk_numeric_permission = cluster_data[i][1]
+            
+            if dbscan_label in perm_dict:
+                perm_dict[dbscan_label].append(apk_numeric_permission)
+            else:
+                perm_dict[dbscan_label] = [apk_numeric_permission] 
+        for dbscan_label in sorted(perm_dict.keys()):
+            f.write("DBSCAN Cluster Number: " + str(dbscan_label) + "\n") #apppend dbscan cluster label
+            f.write("Permission (Numerical) : Frequency" + "\n")
+            freq_count_dict = {}
+            permissions = perm_dict[dbscan_label]
+            for p in permissions:
+                freq_count_dict[p] = permissions.count(p)
+            
+            for p in sorted(freq_count_dict.keys()):
+                f.write(str(p) + " : " + str(freq_count_dict[p]) + "\n") #apppend unique permission freq count
+        f.write(("-"*20) + "\n\n")
+    f.close()
 
 # ==============================
-# TNSE & hierachical implementation
+# TNSE & hierarchical implementation
 # ==============================
 def plot_with_dbscan_labels(tsne_results, labels, unique_grp, plt_label_2):
     sns.scatterplot(
@@ -53,7 +85,7 @@ def apply_tsne(input_data, permission_as_num, unique_grp, label=''):
     print("applying tsne...")
     tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
     tsne_results = tsne.fit_transform(input_data)
-
+    '''
     sns_plot = sns.scatterplot(
         x=tsne_results[:,0], y=tsne_results[:,1],
         palette=sns.color_palette("hls", unique_grp),
@@ -64,7 +96,7 @@ def apply_tsne(input_data, permission_as_num, unique_grp, label=''):
     fig_name = "tsne" + label + ".png"
     plt.savefig(fig_name)
     plt.clf()
-
+    '''
     return tsne_results
 
 def hierarchical_plot(input_data):
@@ -84,16 +116,16 @@ def hierarchical_plot(input_data):
     return cutree
 
 def seperate_clusters(cluster_labels, input_data, permission_labels):
-    cluster_dict = {} #each cluster label has list of tuple - (point, permission)
+    cluster_dict = {} #each cluster label (hierarchical clustering) has list of tuple - (point, permission)
     for i in range(len(cluster_labels)):
         cluster_num = cluster_labels[i][0]
         data_point = input_data[i]
         perm = permission_labels[i]
 
         if cluster_num in cluster_dict:
-            cluster_dict[cluster_num].append((data_point, perm))
+            cluster_dict[cluster_num].append([data_point, perm])
         else:
-            cluster_dict[cluster_num] = [(data_point, perm)]
+            cluster_dict[cluster_num] = [[data_point, perm]]
            
     count_diagrams = 1
     
@@ -113,8 +145,10 @@ def seperate_clusters(cluster_labels, input_data, permission_labels):
         plt_label_1 = "_cluster_" + str(count_diagrams)
         tsne_results = apply_tsne(input_data_list, permission_as_num, unique_grp, plt_label_1)
 
-        labels = apply_clustering(input_data_list, str(count_diagrams)) #ndarray structure
+        # labels: DBSCAN cluster labels
+        labels = apply_clustering(input_data_list, str(count_diagrams)) # ndarray structure
         labels = labels.tolist()
+        
         if min(labels) == -1:
             unique_grp = max(labels) + 2
         else:
@@ -122,16 +156,20 @@ def seperate_clusters(cluster_labels, input_data, permission_labels):
 
         plt_label_2 = "_cluster_" + str(count_diagrams) + "_DBlabels"
 
-        plot_with_dbscan_labels(tsne_results, labels, unique_grp, plt_label_2)
+        #plot_with_dbscan_labels(tsne_results, labels, unique_grp, plt_label_2)
 
         count_diagrams += 1
-    
 
+        #append DBSCAN label to dict
+        for j in range(len(data)):
+            cluster_dict[key][j].append(labels[j])
+
+    return cluster_dict
 # =========================
 # DBSCAN implementation
 # =========================
 def compute_eps(input_data, diag_label = ''):
-    print("computing eps param")
+    print("computing eps param...")
     fig_name = "eps_value" + diag_label + ".png"
 
     neigh = NearestNeighbors(n_neighbors=5)
@@ -154,7 +192,7 @@ def compute_eps(input_data, diag_label = ''):
 def apply_clustering(input_data, diag_label = ''):
     print("applying dbscan...")
     eps_len = compute_eps(input_data)
-    print(eps_len)
+    
     db = DBSCAN(eps=eps_len, min_samples=5).fit(input_data)
     labels = db.labels_
 
@@ -194,7 +232,9 @@ def prepare_apks():
     permission_as_num, unique_grp = tranform_permission(permission_list) #unique group = 44 on whole dataset
     
     cluster_labels = hierarchical_plot(extracted_features) #structure: [array([0]), array([1]),...]
-    seperate_clusters(cluster_labels, extracted_features, permission_as_num)
+    cluster_dict = seperate_clusters(cluster_labels, extracted_features, permission_as_num)
+
+    permission_freq(cluster_dict)
     
 def main():
     prepare_apks()
